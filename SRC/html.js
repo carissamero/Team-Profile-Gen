f98823e10e058c00b9cc8c4adffa22189485b709
carissamero <carissamero@yahoo.com>
2022-05-25 20:29:14 -0500

    updated js

---
+++ SRC/html.js
@@ -0,0 +1,111 @@
+const Employee = require(`../lib/Employee.js`);
+const Manager = require("../lib/Manager.js");
+const Intern = require("../lib/Intern.js");
+const Engineer = require("../lib/Engineer.js");
+
+function createManagerCard(manager) {
+    return `<div class="card m-3" style="width: 18rem;">
+                <div class="card-header bg-primary text-white">
+                    <h4>${manager.getName()}</h4>
+                    <h5>${manager.getRole()}</h5>
+                </div>
+                <div class="card-body" style="background-color:#D9D9D9;">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${manager.getId()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                        <li class="list-group-item">Office: ${
+                          manager.officeNumber
+                        } </li>
+                    </ul>
+                </div>
+            </div>
+    
+            `;
+}
+
+function createEngineerCard(engineer) {
+    return `<div class="card m-3" style="width: 18rem;">
+                <div class="card-header bg-primary text-white">
+                    <h4>${engineer.getName()}</h4>
+                    <h5>${engineer.getRole()}</h5>
+                </div>
+                <div class="card-body" style="background-color:#D9D9D9;">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${engineer.getId()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}<a></li>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+                    </ul>
+                </div>
+            </div>
+    
+            `;
+}
+
+function createInternCard(intern) {
+    return `<div class="card m-3" style="width: 18rem;">
+                <div class="card-header bg-primary text-white">
+                    <h4>${intern.getName()}</h4>
+                    <h5>${intern.getRole()}</h5>
+                </div>
+                <div class="card-body" style="background-color:#D9D9D9;">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${intern.getId()}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                        <li class="list-group-item">School: ${intern.getSchool()}</li>
+                    </ul>
+                </div>
+            </div>
+    
+            `;
+}
+
+function createHTML(staff) {
+    const cards = staff
+        .map((employee) => {
+            if (employee.getRole() == "Manager") {
+                return createManagerCard(employee);
+            } else if (employee.getRole() == "Engineer") {
+                return createEngineerCard(employee);
+            } else if (employee.getRole() == "Intern") {
+                return createInternCard(employee);
+            }
+        })
+        .join("");
+
+    return `<!DOCTYPE html>
+ <html lang="en">
+ 
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <title>Team Profile</title>
+ </head>
+ 
+ <body>
+ 
+     <!-- Header -->
+     <header class="container-fluid bg-danger mb-3 p-2">
+         <h1 class="display-5 text-white text-center">My Team</h1>
+     </header>
+     <!-- Main -->
+     <main class = "container">
+         <div class = "row d-flex justify-content-center">
+ 
+         ${cards}
+ 
+         </div>
+     </main>
+ 
+ <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+ 
+ </body>
+ 
+ </html>
+ `;
+}
+
+module.exports = createHTML;
---
+++ index.js
@@ -0,0 +1,112 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const createHTML = require("./src/html.js");
+
+const Employee = require("./Lib/employee.test.js");
+const Manager = require("./Lib/manager.test.js");
+const Intern = require("./Lib/intern.test.js");
+const Engineer = require("./Lib/engineer.test.js");
+
+const staff = [];
+
+function getEmployeeData(role) {
+    return inquirer
+        .prompt([{
+                type: "input",
+                message: `What is the ${role}'s name? `,
+                name: "name",
+            },
+            {
+                type: "number",
+                message: `What is the ${role}'s ID? `,
+                name: "id",
+            },
+            {
+                type: "input",
+                message: `What is the ${role}'s email? `,
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the manager's office number? ",
+                name: "officeNumber",
+                when: role === "manager",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's GitHub username? ",
+                name: "github",
+                when: role === "engineer",
+            },
+            {
+                type: "input",
+                message: "What is the intern's school? ",
+                name: "school",
+                when: role === "intern",
+            },
+            {
+                type: "list",
+                message: `What do you wish to do next?`,
+                name: "nextStep",
+                choices: ["Add an engineer", "Add an intern", "Build my team"],
+            },
+        ])
+        .then((response) => {
+            let newEmployee = {};
+
+            switch (role) {
+                case "manager":
+                    newEmployee = new Manager(
+                        response.name,
+                        response.id,
+                        response.email,
+                        response.officeNumber
+                    );
+                    break;
+                case "engineer":
+                    newEmployee = new Engineer(
+                        response.name,
+                        response.id,
+                        response.email,
+                        response.github
+                    );
+                    break;
+                case "intern":
+                    newEmployee = new Intern(
+                        response.name,
+                        response.id,
+                        response.email,
+                        response.school
+                    );
+                    break;
+                default:
+                    throw new Error("unknown employee role");
+            }
+
+            // add new employee to array of all employees
+            staff.push(newEmployee);
+
+            if (response.nextStep === "Add an engineer") {
+                return getEmployeeData("engineer");
+            } else if (response.nextStep === "Add an intern") {
+                return getEmployeeData("intern");
+            } else {
+                const html = createHTML(staff);
+                writeToFile(html);
+            }
+        });
+}
+
+function writeToFile(generatedHTML) {
+    fs.writeFile("./dist/index.html", generatedHTML, (err) =>
+        err ? console.log(err) : console.log("Success!")
+    );
+}
+
+function init() {
+    console.log("\nWelcome to the Team Profile Generator.");
+    console.log("Begin by entering manager information.");
+    getEmployeeData("manager");
+}
+
+init();
